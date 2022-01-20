ce5c1d1d39c2a52788c7229ad814b92fa7768c52
Chani Thorne <chani.thorne@go.mnstate.edu>
2022-01-20 12:54:19 -0600

    Completed Lab 2

--- assignments.R
+++ assignments.R
@@ -2,6 +2,33 @@
 library(tidyverse)
 library(palmerpenguins)
 
+Question 10C
+
 ggplot(data = penguins) + 
   geom_point(mapping = aes(x = body_mass_g, y = flipper_length_mm))
 
+Question 11B
+
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g,
+                           y = flipper_length_mm,
+                           color = species, 
+                           shape = island)) 
+ 
+Question 13B          
+                                                     
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g,
+                           y = flipper_length_mm,
+                           color = species, 
+                           shape = island))+ 
+  facet_wrap(~ species, nrow = 1)
+
+Question 13C
+
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g,
+                           y = flipper_length_mm,
+                           color = species, 
+                           shape = island))+ 
+  facet_grid(species~ island)
--- examples.R
+++ examples.R
@@ -9,5 +9,13 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ , y = hwy))
 
 ggplot(data = mpg) + 
-  GEOM_FUNCTION>(mapping = aes(MAPPINGS))
+  geom_point(mapping = aes(x = displ, y = hwy,
+                           color = class, shape = class))
 
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(drv ~ cyl)
